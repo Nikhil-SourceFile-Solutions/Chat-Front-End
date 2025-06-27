06177d4d4c983f344e379a1922ff24df36593b3d
Nikhil Sasikumar <nikhil@sourcefilesolutions.com>
2025-06-27 17:37:52 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
 
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
+    <Router>
+    
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   )
 }
 
